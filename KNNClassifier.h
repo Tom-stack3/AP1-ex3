6cd39f3805e5ccc116a267c6d44231889908cd45
Tom-stack3 <tommy.zaft1@gmail.com>
2021-07-27 11:19:58 +0300

    added comments

--- KNNClassifier.h
+++ KNNClassifier.h
@@ -4,10 +4,17 @@ class KNNClassifier
 {
 private:
     std::vector<Flower> data;
+    /**
+     * Find and return the closest K flowers.
+     */
     std::vector<Flower> findKNN(const int k, const Flower &f);
 
 public:
-    // Overiding the interface function.
     KNNClassifier(std::vector<Flower> data);
+
+    /**
+     * Return a label predicted for the Flower.
+     * (Overiding the interface function)
+     */
     std::string predict(const Flower &f);
 };
