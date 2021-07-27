7dcbcc7f55bda878a7c7e436ee59d593052c512e
Tom-stack3 <tommy.zaft1@gmail.com>
2021-07-27 13:39:00 +0300

    code reformat using vscode

--- KNNClassifier.cpp
+++ KNNClassifier.cpp
@@ -36,20 +36,28 @@ std::string KNNClassifier::predict(const Flower &flower)
 {
     int k = 5;
     std::vector<Flower> knnFlowers = findKNN(k, flower);
-    int result [] {0,0,0};
+    int result[]{0, 0, 0};
     for (int i = 0; i < k; i++)
     {
-        if(knnFlowers[i].getLabel().compare("Iris-setosa") == 0){result [0]++;}
-        if(knnFlowers[i].getLabel().compare("Iris-virginica") == 0){result [1]++;}
-        if(knnFlowers[i].getLabel().compare("Iris-versicolor") == 0){result [2]++;}        
+        if (knnFlowers[i].getLabel().compare("Iris-setosa") == 0)
+        {
+            result[0]++;
+        }
+        else if (knnFlowers[i].getLabel().compare("Iris-virginica") == 0)
+        {
+            result[1]++;
+        }
+        else if (knnFlowers[i].getLabel().compare("Iris-versicolor") == 0)
+        {
+            result[2]++;
+        }
     }
 
-    int max = result[0] > ( result[1] > result[2] ? result[1] : result[2]) ? result[0] : ( result[1] > result[2] ? result[1] : result[2]);
+    int max = result[0] > (result[1] > result[2] ? result[1] : result[2]) ? result[0] : (result[1] > result[2] ? result[1] : result[2]);
 
-    if(result[0] == max)
+    if (result[0] == max)
         return "Iris-setosa";
-    if(result[1] == max)
+    if (result[1] == max)
         return "Iris-virginica";
     return "Iris-versicolor";
-
 }
