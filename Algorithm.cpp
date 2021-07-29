ac2664263f033d0a4cfacb5d91696d8c24f6a0bf
Idan <idantur06@gmail.com>
2021-07-29 22:29:26 +0300

    added Algorithem.h & .c class. (not implemented yet)

---
+++ Algorithm.cpp
@@ -0,0 +1,17 @@
+#include "Algorithm.h"
+
+
+template<class T>
+std::vector<T> Algorithm<T>::select(int k, std::vector<T> arr, int l , int r){
+    if(r == -1){
+        r = arr.size();
+    }
+    return nullptr;
+}
+
+template<class T>
+void Algorithm<T>::partition(std::vector<T> arr,int x ,int l , int r){
+    if(r == -1){
+            r = arr.size();
+    }
+}
