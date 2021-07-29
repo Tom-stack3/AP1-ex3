4bf94914add84cb54a721369cf1fbd2db4eaa71c
Idan <idantur06@gmail.com>
2021-07-29 22:32:16 +0300

    refactored Algorithm.cpp & .h

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -1,17 +1,20 @@
 #include "Algorithm.h"
 
-
-template<class T>
-std::vector<T> Algorithm<T>::select(int k, std::vector<T> arr, int l , int r){
-    if(r == -1){
+template <class T>
+std::vector<T> Algorithm<T>::select(int k, std::vector<T> arr, int l, int r)
+{
+    if (r == -1)
+    {
         r = arr.size();
     }
     return nullptr;
 }
 
-template<class T>
-void Algorithm<T>::partition(std::vector<T> arr,int x ,int l , int r){
-    if(r == -1){
-            r = arr.size();
+template <class T>
+void Algorithm<T>::partition(std::vector<T> arr, T x, int l, int r)
+{
+    if (r == -1)
+    {
+        r = arr.size();
     }
 }
--- Algorithm.h
+++ Algorithm.h
@@ -1,18 +1,16 @@
 #ifndef ALGORITHM_H
 #define ALGORITHM_H
 #include <vector>
-template<class T>
+template <class T>
 
 class Algorithm
 {
 public:
     // return k smallest elements.
-    std::vector<T> select(int k, std::vector<T> arr , int l = 0, int r = -1);
+    std::vector<T> select(int k, std::vector<T> arr, int l = 0, int r = -1);
 
     // sort the array based on the comparation with x. (smaller then x will be on the left of x, and the rest on the right of x)
-    void partition(std::vector<T> arr, T x , int l = 0, int r = -1);
-
-
+    void partition(std::vector<T> arr, T x, int l = 0, int r = -1);
 };
 
 #endif
