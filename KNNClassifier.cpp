90cbbf026c9d4eaa1f55fa97adecb5e03085fa0a
Tom-stack3 <tommy.zaft1@gmail.com>
2021-07-30 21:33:26 +0300

    deleted unused findKNN()

--- KNNClassifier.cpp
+++ KNNClassifier.cpp
@@ -10,35 +10,11 @@ void KNNClassifier::setK(const int k){
     m_k = k;
 }
 
-std::vector<Flower> KNNClassifier::findKNN(const int k, const Flower &flower)
-{
-  // Sotring the array
-    for (int i = 0; i < m_data.size(); i++)
-    {
-        for (int j = 0; j < m_data.size() - 1; j++)
-        {
-            if (m_data[j].getDistance(flower) > m_data[j + 1].getDistance(flower))
-            {
-                Flower temp(m_data[j]);
-                m_data[j] = m_data[j + 1];
-                m_data[j + 1] = temp;
-            }
-        }
-    }
-
-    std::vector<Flower> flowers;
-    for (int i = 0; i < k; i++)
-    {
-        flowers.push_back(m_data[i]);
-    }
-    return flowers;
-}
-
 std::string KNNClassifier::predict(const Flower &flower)
 {
-    Algorithm<Flower> algo{};
+    Algorithm<Flower> algo;
     std::vector<Flower> knnFlowers = algo.getKSmallest(algo.sortByDiffrence(m_data, flower), m_k);
-    //std::vector<Flower> knnFlowers = findKNN(k, flower);
+
     int result[]{0, 0, 0};
     for (int i = 0; i < m_k; i++)
     {
--- KNNClassifier.h
+++ KNNClassifier.h
@@ -11,10 +11,6 @@ private:
     std::vector<Flower> m_data;
     // the K we want the prediction to work with.
     int m_k;
-    /**
-     * Find and return the closest K flowers.
-     */
-    std::vector<Flower> findKNN(const int k, const Flower &f);
 
 public:
     KNNClassifier(std::vector<Flower> data, const int k = 5);
