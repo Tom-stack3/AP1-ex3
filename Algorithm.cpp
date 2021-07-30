2c2b556886f6c16f2137c6794a7f3d8291a99b94
Tom-stack3 <tommy.zaft1@gmail.com>
2021-07-30 21:55:54 +0300

    moved code fro, .h to .cpp file also trying Method 1 to solve the compile error :(

---
+++ Algorithm.cpp
@@ -0,0 +1,33 @@
+#include "Algorithm.h"
+#include "Flower.h"
+
+template <class T>
+std::vector<T> Algorithm<T>::sortByDiffrence(std::vector<T> v, T x)
+{
+    std::multimap<int, T> m;
+    // Store values in a multimap with the difference
+    // with X as key
+    for (int i = 0; i < v.size(); i++)
+        m.insert(std::make_pair(abs(v[i].getDistance(x)), v[i]));
+
+    // Update the values of array
+    int i = 0;
+    for (auto const &x : m)
+    {
+        v[i++] = x.second;
+    }
+    return v;
+}
+
+template <class T>
+std::vector<T> Algorithm<T>::getKSmallest(std::vector<T> sorted, int k)
+{
+    std::vector<T> kSmallest(sorted.begin(), sorted.begin() + k);
+    return kSmallest;
+}
+
+//trying method 1
+void TemporaryFunction ()
+{
+    Algorithm<Flower> TempObj;
+}
--- Algorithm.h
+++ Algorithm.h
@@ -10,31 +10,14 @@ class Algorithm
 {
 public:
     /**
+     * Sort the vector.
+     */
+    std::vector<T> sortByDiffrence(std::vector<T> v, T x);
+
+    /**
      * Return the K smallest elements.
      */
-    std::vector<T> sortByDiffrence(std::vector<T> v, T x)
-    {
-        std::multimap<int, T> m;
-        typename std::multimap<int, T>::iterator it;
-        // Store values in a map with the difference
-        // with X as key
-        for (int i = 0; i < v.size(); i++)
-            m.insert(std::make_pair(abs(v[i].getDistance(x)), v[i]));
-
-        // Update the values of array
-        int i = 0;
-        for (auto const &x : m)
-        {
-            v[i++] = x.second;
-        }
-        return v;
-    }
-
-    std::vector<T> getKSmallest(std::vector<T> sorted, int k)
-    {
-        std::vector<T> kSmallest(sorted.begin(), sorted.begin() + k);
-        return kSmallest;
-    }
+    std::vector<T> getKSmallest(std::vector<T> sorted, int k);
 };
 
 #endif
