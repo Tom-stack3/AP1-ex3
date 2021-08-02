6ced7414c1f0de0964762654f8f44293d9facb05
Tom-stack3 <tommy.zaft1@gmail.com>
2021-08-02 14:42:12 +0300

    added classified

---
+++ Classified.h
@@ -0,0 +1,42 @@
+#ifndef CLASSIFIED_H
+#define CLASSIFIED_H
+#include <string>
+#include <vector>
+#include <math.h>
+class Classified
+{
+private:
+    // the properties of the classified object.
+    std::vector<double> m_properties;
+    // the label of the classified object.
+    std::string m_label;
+
+public:
+    // constructors
+    Classified(const Classified &f);
+    Classified(std::vector<double> properties, std::string label = "");
+
+    Classified &operator=(const Classified &classified);
+
+    /**
+     * Return a const reference to the properties of the classified object.
+     */
+    const std::vector<double> &getProperties() const;
+
+    /**
+     * Return a const reference to the label of the classified object.
+     */
+    const std::string &getLabel() const;
+
+    /**
+     * Set a new label.
+     */
+    void setLabel(std::string newLabel);
+
+    /**
+     * Get distance between this classified object and other classified object.
+     */
+    virtual double getDistance(const Classified &f) const=0;
+};
+
+#endif
