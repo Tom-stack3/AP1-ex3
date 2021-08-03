407f7d96f1f8b9f7ee4d69c89155ece73a10812f
Tom-stack3 <tommy.zaft1@gmail.com>
2021-08-03 10:17:38 +0300

    made getKSmallest() simpler

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -4,7 +4,7 @@ template <class T>
 void Algorithm<T>::sortByDiffrence(std::vector<std::shared_ptr<T>>& v, const T &x)
 {
     // last thing to implement
-    
+
     /*std::multimap<double, T&> m;
     // Store values in a multimap with the difference
     // with X as key
@@ -23,19 +23,7 @@ void Algorithm<T>::sortByDiffrence(std::vector<std::shared_ptr<T>>& v, const T &
 template <class T>
 std::vector<std::shared_ptr<T>> Algorithm<T>::getKSmallest(std::vector<std::shared_ptr<T>> &sorted, int k)
 {
-    std::vector<std::shared_ptr<T>> kSmallest;
-    for (int i = 0; i < k; i++)
-    {
-        kSmallest.push_back(sorted[i]);
-    }
-
-    /*std::vector<std::shared_ptr<typename T>>::iterator i;
-    for (i = sorted.begin(); i < sorted.begin() + k; i++)
-    {
-        //kSmallest.push_back(**i);
-    }*/
-    //std::vector<T> kSmallest(sorted.begin(), sorted.begin() + k);
-
+    std::vector<std::shared_ptr<T>> kSmallest(sorted.begin(), sorted.begin() + k);
     return kSmallest;
 }
 
