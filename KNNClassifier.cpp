7c1a5c318ffc7477c41922f311d3d8663395f2cc
Idan <idantur06@gmail.com>
2021-08-12 22:52:25 +0300

    added general max-picking

--- KNNClassifier.cpp
+++ KNNClassifier.cpp
@@ -1,12 +1,13 @@
 #include "KNNClassifier.h"
 
-KNNClassifier::KNNClassifier(std::vector<std::shared_ptr<Classified>>& classifiedObjects, int k)
+KNNClassifier::KNNClassifier(std::vector<std::shared_ptr<Classified>> &classifiedObjects, int k)
 {
     m_data = classifiedObjects;
     m_k = k;
 }
 
-void KNNClassifier::setK(const int k){
+void KNNClassifier::setK(const int k)
+{
     m_k = k;
 }
 
@@ -16,29 +17,27 @@ std::string KNNClassifier::predict(const Classified &cls)
 
     algo.sortByDiffrence(m_data, cls);
     std::vector<std::shared_ptr<Classified>> knnFlowers = algo.getKSmallest(m_data, m_k);
+    // return the string that apperes max times in the array
 
-    int result[]{0, 0, 0};
-    for (int i = 0; i < m_k; i++)
+    // stores the amount each lable apperes in the array.
+
+    // creates a map with the key as the lables, and the value as the number of apperences in the array.
+    std::map<std::string, int> map;
+    for (std::shared_ptr<Classified> &classified : knnFlowers)
     {
-        if (knnFlowers[i]->getLabel().compare("Iris-setosa") == 0)
-        {
-            result[0]++;
-        }
-        else if (knnFlowers[i]->getLabel().compare("Iris-virginica") == 0)
-        {
-            result[1]++;
-        }
-        else if (knnFlowers[i]->getLabel().compare("Iris-versicolor") == 0)
-        {
-            result[2]++;
-        }
+        map[classified->getLabel()]++;
     }
 
-    int max = result[0] > (result[1] > result[2] ? result[1] : result[2]) ? result[0] : (result[1] > result[2] ? result[1] : result[2]);
-
-    if (result[0] == max)
-        return "Iris-setosa";
-    if (result[1] == max)
-        return "Iris-virginica";
-    return "Iris-versicolor";
+    // get the string that apperes max times in the map
+    std::string maxString;
+    int max = 0;
+    
+    for (auto const &x : map)
+    {
+        if(x.second > max){
+            max = x.second;
+            maxString = x.first;
+        }
+    }
+    return maxString;
 }
