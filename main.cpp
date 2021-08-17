d82b4f69b1d1cbf4f28af8ed5e9d3e16588d7da3
Tom-stack3 <tommy.zaft1@gmail.com>
2021-08-17 16:45:48 +0300

    deleted old code comment

--- main.cpp
+++ main.cpp
@@ -2,37 +2,6 @@
 
 int main()
 {
-    /*
-    // Create a new Reader instance
-    Reader r = Reader(std::string("../data/classified.csv"));
-    // Read and load the Classified Flowers
-    std::vector<std::shared_ptr<Classified>> classifiedObjects;
-    r.read(classifiedObjects);
-
-    // Change the reader path to the path of the Unclassified Flowers
-    r.setInputPath(std::string("../data/Unclassified.csv"));
-    // Read and load the Unclassified Flowers
-    std::vector<std::shared_ptr<Classified>> unclassifiedFlowers;
-    r.read(unclassifiedFlowers);
-
-    const int k = 5;
-    // Create a new KNNClassifier
-    KNNClassifier knn(classifiedObjects, k);
-    Classifier& classifier = knn;
-
-    for (const std::shared_ptr<Classified>& f : unclassifiedFlowers)
-    {
-        // Set the classified object label to the prediction the Model made
-        f->setLabel(classifier.predict(*f));
-    }
-
-    // Create a new Writer
-    Writer w = Writer(std::string("../output.csv"));
-    // Write the newly classified Flowers to the output file.
-    // The flowers in "unclassifiedFlowers" vector are now classified.
-    w.write(unclassifiedFlowers);
-    */
-
     Tcp s{};
     Socket &server = s;
 
