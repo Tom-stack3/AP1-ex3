2d0e1cee386b5aa1914f5d020eb6912121b888e0
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-05 20:43:05 +0300

    added Euclidean distance

---
+++ distance/EucDistance.cpp
@@ -0,0 +1,14 @@
+#include "EucDistance.h"
+#include <iostream>
+double EucDistance::getDist(const Classified &cls1, const Classified &cls2){
+    std::cout<<"Calc_  _"<<std::endl;
+    std::vector<double> properites1 = cls1.getProperties();
+    std::vector<double> properites2 = cls2.getProperties();
+    double euclideanDist = 0;
+    for (int i = 0; i < properites1.size(); i++)
+    {
+        euclideanDist += pow(properites2[i] - properites1[i], 2);
+    }
+
+    return sqrt(euclideanDist);
+}
---
+++ distance/EucDistance.h
@@ -0,0 +1,17 @@
+#ifndef EUC_DISTANCE_H
+#define EUC_DISTANCE_H
+#include "../Classified.h"
+#include <math.h>
+
+class Classified; // forward declaration 
+
+class EucDistance
+{
+public:
+    /**
+     * Get euclidean distance between two classified objects.
+     */
+    static double getDist(const Classified &cls1, const Classified &cls2);
+};
+
+#endif
