d6edf2961973b1de73e8b9934496e3826f00082a
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-06 22:49:09 +0300

    added messages printing

--- server/commands/ClassifyCommand.cpp
+++ server/commands/ClassifyCommand.cpp
@@ -6,9 +6,18 @@ void ClassifyCommand::execute()
     std::vector<std::shared_ptr<Classified>> trained = d->getTrainData();
     std::vector<std::shared_ptr<Classified>> toClassify = d->getClassifiedData();
 
+    // If one of the train vector is empty
+    if (trained.empty())
+    {
+        getDIO()->write("please upload train data");
+        return;
+    }
+
     // Create a new KNNClassifier
     KNNClassifier knn(trained, d->getK());
     Classifier &classifier = knn;
 
     classifier.predict(toClassify);
+
+    getDIO()->write("classifying data complete");
 }
