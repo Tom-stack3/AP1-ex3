67fd181c1bdba46cad0196d087e32de656d732e1
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-06 16:48:57 +0300

    added predict to a vector function. overlaod of predict().

--- classifier/Classifier.h
+++ classifier/Classifier.h
@@ -10,5 +10,10 @@ public:
      * Return a label predicted for a Classified object.
      */
     virtual std::string predict(const Classified &f) = 0;
+
+    /**
+     * Classify the vector.
+     */
+    virtual void predict(std::vector<std::shared_ptr<Classified>>);
 };
 #endif
