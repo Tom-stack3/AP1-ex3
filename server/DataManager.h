25b16e0e4369d3684afdce1e2f03183c874ff4d7
turkeni <idantur06@gmail.com>
2021-09-06 12:21:48 +0000

    added getters and setters for Data Manager

--- server/DataManager.h
+++ server/DataManager.h
@@ -9,15 +9,34 @@
 class DataManager
 {
 private:
-    int m_K;
+    int m_k;
     std::vector<std::shared_ptr<Classified>> m_trainData;
     std::vector<std::shared_ptr<Classified>> m_testData;
     std::vector<std::shared_ptr<Classified>> m_classifiedData;
     double (*m_dist)(const Classified &, const Classified &);
 
-
 public:
     typedef double (*distMetric)(const Classified &, const Classified &);
+    
+    // getter and setter for k
+    int getK();
+    void setK(int k);
+
+    // getter and setter for train data
+    std::vector<std::shared_ptr<Classified>> getTrainData();
+    void setTrainData(std::vector<std::shared_ptr<Classified>> data);
+
+    // getter and setter for test data
+    std::vector<std::shared_ptr<Classified>> getTestData();
+    void setTestData(std::vector<std::shared_ptr<Classified>> data);
+
+    // getter and setter for classified data
+    std::vector<std::shared_ptr<Classified>> getClassifiedData();
+    void setClassifiedData(std::vector<std::shared_ptr<Classified>> data);
+
+    // getter and setter for dist function pointer
+    distMetric getDistMetric();
+    void setDistMetric(distMetric ds);
 };
 
 #endif
