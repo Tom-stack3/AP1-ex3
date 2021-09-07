8b54d7aaa31c038a4c51d461a33bfd95f2b864f0
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-07 21:24:28 +0300

    deleted: const static int NUM_OF_PROPERTIES

--- classifier/Classified.h
+++ classifier/Classified.h
@@ -23,8 +23,6 @@ private:
 public:
     typedef double (*distMetric)(const Classified &, const Classified &);
 
-    const static int NUM_OF_PROPERTIES = 4;
-
     // constructors
     Classified(const Classified &f);
     Classified(std::vector<double> properties, distMetric *distMetric, std::string label = "");
