9d8222a07341296255a5085b8ff252d3711810ea
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-07 17:34:25 +0300

    created Input Validator

---
+++ utils/InputValidator.h
@@ -0,0 +1,20 @@
+#ifndef INPUT_VALIDATOR_H
+#define INPUT_VALIDATOR_H
+
+#include <sstream>
+#include <algorithm>
+
+class InputValidator
+{
+public:
+    /**
+     * Returns true if the str is a valid double, Otherwise - returns false.
+     */
+    static bool isDouble(std::string const &str);
+    /**
+     * Returns true if the str is a valid integer, Otherwise - returns false.
+     */
+    static bool isInt(std::string const &str);
+};
+
+#endif
