454108572cff981c3d17fb475f53aab51379a7e8
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-07 23:10:10 +0300

    created StringFunctions added replaceChar() and removeChar()

---
+++ utils/StringFunctions.cpp
@@ -0,0 +1,40 @@
+#include "StringFunctions.h"
+
+std::vector<std::string> StringFunctions::split(std::string const &strToSplit, std::string const &delimiter)
+{
+    std::vector<std::string> strSplitted;
+
+    // Split the string by a comma and save the parts in the vector strSplitted.
+    int start = 0;
+    int end = strToSplit.find(delimiter);
+    while (end != -1)
+    {
+        strSplitted.push_back(strToSplit.substr(start, end - start));
+        start = end + delimiter.size();
+        end = strToSplit.find(delimiter, start);
+    }
+    strSplitted.push_back(strToSplit.substr(start, end - start));
+    return strSplitted;
+}
+
+std::string StringFunctions::replaceChar(const std::string &str, char oldChar, char newChar)
+{
+    std::string newStr = str;
+    for (int i = 0; i < str.length(); ++i)
+    {
+        if (newStr[i] == oldChar)
+            newStr[i] = newChar;
+    }
+
+    return newStr;
+}
+
+std::string StringFunctions::removeChar(const std::string &str, char c)
+{
+    std::string output;
+    output.reserve(str.size()); // avoids buffer reallocations in the loop
+    for (size_t i = 0; i < str.size(); ++i)
+        if (str[i] != c)
+            output += str[i];
+    return output;
+}
---
+++ utils/StringFunctions.h
@@ -0,0 +1,26 @@
+#ifndef STRING_FUNCTIONS_H
+#define STRING_FUNCTIONS_H
+
+#include <string>
+#include <vector>
+
+class StringFunctions
+{
+public:
+    /**
+     * Split the string recieved by the delimiter.
+     */
+    static std::vector<std::string> split(std::string const &strToSplit, std::string const &delimiter = " ");
+
+    /**
+     * Replace a char by a char and return the new string.
+     */
+    static std::string replaceChar(const std::string &str, char oldChar, char newChar);
+
+    /**
+     * Remove a char from a string and return the new string.
+     */
+    static std::string removeChar(const std::string &str, char oldChar);
+};
+
+#endif
