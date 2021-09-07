19380356ef74bd0ce501705de29651940a379495
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-07 21:23:12 +0300

    made Reader work with a general number of properties (discovered from the first line of the file or the string). Added a read function that inits from a string and not a file. Made Reader recieve a distMetic* on initialization.

--- utils/Reader.cpp
+++ utils/Reader.cpp
@@ -1,8 +1,9 @@
 #include "Reader.h"
 
-Reader::Reader(std::string inputPath)
+Reader::Reader(std::string inputPath, Classified::distMetric *distMetric)
 {
     m_inputPath = std::move(inputPath);
+    m_distMetric = distMetric;
 }
 
 void Reader::setInputPath(std::string inputPath)
@@ -13,38 +14,54 @@ void Reader::setInputPath(std::string inputPath)
 std::shared_ptr<Classified> Reader::parseLine(std::string &line) const
 {
     const std::string comma = ",";
-    std::vector<std::string> strSplitted;
-
-    // Split the string by a comma and save the parts in the vector strSplitted.
-    int start = 0;
-    int end = line.find(comma);
-    while (end != -1)
-    {
-        strSplitted.push_back(line.substr(start, end - start));
-        start = end + comma.size();
-        end = line.find(comma, start);
-    }
-    strSplitted.push_back(line.substr(start, end - start));
+    std::vector<std::string> strSplitted = Split::split(line, comma);
 
     // If the row doesn't have a label, we add an empty one.
-    if (strSplitted.size() == Classified::NUM_OF_PROPERTIES)
+    if (strSplitted.size() == m_numOfProperties)
     {
         strSplitted.push_back(std::string(""));
     }
     std::vector<double> properties;
-    for (int i = 0; i < Classified::NUM_OF_PROPERTIES; i++)
+    for (int i = 0; i < m_numOfProperties; i++)
     {
         // String to double
         properties.push_back(std::stod(strSplitted.at(i)));
     }
-    return std::make_shared<Classified>(properties, strSplitted.at(4));
+    return std::make_shared<Classified>(properties, m_distMetric, strSplitted.at(m_numOfProperties));
 }
 
-void Reader::read(std::vector<std::shared_ptr<Classified>> &v) const
+void Reader::initNumOfProperties(std::string firstLine)
+{
+    const std::string comma = ",";
+    std::vector<std::string> strSplitted = Split::split(firstLine, comma);
+    int len = strSplitted.size();
+    int numOfProperties = 0;
+    for (auto p : strSplitted)
+    {
+        if (InputValidator::isDouble(p))
+        {
+            numOfProperties++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    m_numOfProperties = numOfProperties;
+}
+
+void Reader::read(std::vector<std::shared_ptr<Classified>> &v)
 {
     std::string line;
     // Open the input file.
     std::ifstream fileRead(m_inputPath);
+
+    // Read the first line
+    getline(fileRead, line);
+    // init the num of properties the classified objects have based on the first line of the file
+    initNumOfProperties(line);
+    // Add a classified to the vector.
+    v.push_back(parseLine(line));
 
     while (getline(fileRead, line))
     {
@@ -52,6 +69,25 @@ void Reader::read(std::vector<std::shared_ptr<Classified>> &v) const
         v.push_back(parseLine(line));
     }
     fileRead.close();
+}
+
+void Reader::read(std::vector<std::shared_ptr<Classified>> &v, std::string input)
+{
+    std::string line;
+    std::istringstream f(input);
+
+    // Read the first line
+    getline(f, line);
+    // init the num of properties the classified objects have based on the first line of the file
+    initNumOfProperties(line);
+    // Add a classified to the vector.
+    v.push_back(parseLine(line));
+
+    while (getline(f, line))
+    {
+        // Add a classified to the vector.
+        v.push_back(parseLine(line));
+    }
 }
 
 std::string Reader::toString()
--- utils/Reader.h
+++ utils/Reader.h
@@ -1,6 +1,9 @@
 #ifndef READER_H
 #define READER_H
 #include "../classifier/Classified.h"
+#include "Split.h"
+#include "InputValidator.h"
+
 #include <vector>
 #include <fstream>
 #include <memory>
@@ -8,14 +11,23 @@
 class Reader
 {
 private:
+    int m_numOfProperties;
+
     std::string m_inputPath;
+    Classified::distMetric *m_distMetric;
+
+    /**
+     * Initialize the number of properties the classified objects have based on the first line of the file.
+     */
+    void initNumOfProperties(std::string firstLine);
+
     /**
      * Parse a line into a Classified object.
      */
     std::shared_ptr<Classified> parseLine(std::string &line) const;
 
 public:
-    Reader(std::string inputPath);
+    Reader(std::string inputPath, Classified::distMetric *distMetric);
 
     /**
      * Set a new input path.
@@ -25,7 +37,13 @@ public:
     /**
      * Read and initiazlize the classes.
      */
-    void read(std::vector<std::shared_ptr<Classified>> &v) const;
+    void read(std::vector<std::shared_ptr<Classified>> &v);
+
+    /**
+     * Read and initiazlize the classes from a string.
+     */
+    void read(std::vector<std::shared_ptr<Classified>> &v, std::string input);
+
 
     /**
      * Return a string of the file read.
