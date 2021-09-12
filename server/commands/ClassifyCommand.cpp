80987738a35e8c4dfdae46c5c1c98483a0229aab
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-12 22:32:43 +0300

    added creation of new classifed vector when Classifying (if there is a new data uploaded, we init the classified vector to be empty)

--- server/commands/ClassifyCommand.cpp
+++ server/commands/ClassifyCommand.cpp
@@ -17,6 +17,24 @@ void ClassifyCommand::execute()
     KNNClassifier knn(trained, d->getK());
     Classifier &classifier = knn;
 
+    // If the toClassify vector is empty - meaning we didn't classify the objects yet.
+    if (toClassify.empty())
+    {
+        // We create a new vector, from the test data, with empty labels.
+        std::vector<std::shared_ptr<Classified>> test = d->getTestData();
+        for (auto const & cls : test)
+        {
+            // Create a copy of the Classified object.
+            std::shared_ptr<Classified> newCls = std::make_shared<Classified>(*cls);
+            // Delete the previous label
+            (*newCls).deleteLabel();
+            // Push to the toClassify vector
+            toClassify.push_back(newCls);
+        }
+        // Set the newly created toClassify vector to be the toClassify vector in the Data Manager.
+        d->setClassifiedData(toClassify);
+    }
+
     classifier.predict(toClassify);
 
     getDIO()->write("classifying data complete");
