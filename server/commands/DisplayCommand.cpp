487d604c03641a3fa357e0abaebb4b8ed6ff8325
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-13 10:30:23 +0300

    Revert "added error msg if didn't classify the data first"
    This reverts commit 71979cf2d0af07a8860fdf1381be0ef8db9e9b65.

--- server/commands/DisplayCommand.cpp
+++ server/commands/DisplayCommand.cpp
@@ -5,15 +5,6 @@ void DisplayCommand::execute()
     DataManager *d = getDataManager();
     // The results to display
     std::vector<std::shared_ptr<Classified>> classified = d->getClassifiedData();
-
-
-    // If the classified vector is empty - meaning we didn't classify the objects yet.
-    if (classified.empty())
-    {
-        getDIO()->write("please classify the data uploaded");
-        return;
-    }
-
     int i = 1;
     // Display the results
     for (auto const &f : classified)
