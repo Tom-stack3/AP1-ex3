c000c7f8625702d6d344c35f6204d8938a79c898
turkeni <idantur06@gmail.com>
2021-09-15 09:46:38 +0000

    added SocketIO.cpp

---
+++ IO/SocketIO.cpp
@@ -0,0 +1,21 @@
+#include "SocketIO.h"
+
+SocketIO::SocketIO(Tcp *s){
+    setSocket(s);
+}
+
+void SocketIO::setSocket(Tcp *s){
+    socket = s;
+}
+
+std::string SocketIO::read() const
+{
+    char buffer[10000];
+    getSocket()->recvSocket(buffer, sizeof(buffer));
+    return buffer;
+}
+
+void SocketIO::write(std::string str) const
+{
+    getSocket()->sendSocket(str);
+}
