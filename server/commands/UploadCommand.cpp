b3ddbdc3e180ab5cac219a3fd66af5c18384c36d
turkeni <idantur06@gmail.com>
2021-09-15 13:28:14 +0000

    changed upload command

--- server/commands/UploadCommand.cpp
+++ server/commands/UploadCommand.cpp
@@ -11,20 +11,35 @@ void UploadCommand::execute()
     if (!getDIO()->getIOKind().compare(STANDARD_KIND))
     {
         // know that the input is a path
-        DataManager *m =  getDataManager();
-        Reader trainReader(train, m->getDistMetric());
-        Reader testReader(test, m->getDistMetric());
+        DataManager *m = getDataManager();
+        Reader trainReader = Reader(train, m->getDistMetric());
+        Reader testReader = Reader(test, m->getDistMetric());
 
-        // doing this to avoid compilation problems.
-        std::vector<std::shared_ptr<Classified>> trainVector = (m->getTrainData());
-        std::vector<std::shared_ptr<Classified>> testVector = (m->getTestData());
+        std::vector<std::shared_ptr<Classified>> trainVector;
+        std::vector<std::shared_ptr<Classified>> testVector;
 
         trainReader.read(trainVector);
         testReader.read(testVector);
+
+        m->setTestData(testVector);
+        m->setTrainData(trainVector);
     }
 
     if (!getDIO()->getIOKind().compare(SOCKET_KIND))
     {
         // know that the input is a string contains the contents of a file
+        DataManager *m = getDataManager();
+
+        Reader trainReader = Reader(m->getDistMetric());
+        Reader testReader = Reader(m->getDistMetric());
+
+        std::vector<std::shared_ptr<Classified>> trainVector;
+        std::vector<std::shared_ptr<Classified>> testVector;
+
+        trainReader.read(trainVector, train);
+        testReader.read(testVector, test);
+
+        m->setTestData(testVector);
+        m->setTrainData(trainVector);
     }
 }
