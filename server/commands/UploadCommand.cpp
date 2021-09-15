8355afd8523b15c665ac272fe9b21802f7b261cb
turkeni <idantur06@gmail.com>
2021-09-15 10:21:08 +0000

    started working on upload commend

--- server/commands/UploadCommand.cpp
+++ server/commands/UploadCommand.cpp
@@ -2,4 +2,14 @@
 
 void UploadCommand::execute()
 {
+    getDIO()->write("Please upload your local train CSV file.");
+    if (getDIO()->getIOKind().compare(STANDARD_KIND))
+    {
+        // know that the input is a path
+    }
+
+    if (getDIO()->getIOKind().compare(SOCKET_KIND))
+    {
+        // know that the input is a string contains the contents of a file
+    }
 }
