28fa3e5b77473d7f141323642d5158c920b2769a
idanturkenits <idantur06@gmail.com>
2021-09-17 15:34:01 +0000

    changed a little

--- server/commands/DisplayCommand.cpp
+++ server/commands/DisplayCommand.cpp
@@ -9,18 +9,21 @@ void DisplayCommand::execute()
     // If the classified vector is empty - meaning we didn't classify the objects yet.
     if (classified.empty())
     {
-        getDIO()->write("please classify the data uploaded");
+        getDIO()->write("please classify the data uploaded, press ENTER to return to main menu\n");
+        getDIO()->read();
         return;
     }
 
     int i = 1;
     // Display the results
+    std::string list = "";
     for (auto const &f : classified)
     {
-        getDIO()->write(std::to_string(i) + "\t" + f->getLabel());
+        list += (std::to_string(i) + "\t" + f->getLabel() + "\n");
         i++;
     }
-    getDIO()->write("Done.");
+
+    getDIO()->write(list + "Done.\npress ENTER to return to main menu\n");
     // Wait for the user to press "Enter", then continue to the Commands menu
     getDIO()->read();
 }
