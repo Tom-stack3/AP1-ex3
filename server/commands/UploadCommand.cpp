f2bccc120a7a9857c1638d3c5b4045f353a48072
idanturkenits <idantur06@gmail.com>
2021-09-17 15:35:19 +0000

    added printing to see

--- server/commands/UploadCommand.cpp
+++ server/commands/UploadCommand.cpp
@@ -2,18 +2,20 @@
 
 void UploadCommand::execute()
 {
-    getDIO()->write("Please upload your local train CSV file.");
+    getDIO()->write("Please upload your local train CSV file.ֿ");
+
     std::string train = getDIO()->read();
+    std::cout << train << std::endl;
 
     getDIO()->write("Please upload your local test CSV file.");
     std::string test = getDIO()->read();
+    std::cout << "Test:\n" + test << std::endl;
 
     DataManager *m = getDataManager();
 
     if (!getDIO()->getIOKind().compare(StandardIO::KIND))
     {
-        // The input is a path
-
+        // know that the input is a path
         Reader trainReader = Reader(train, m->getDistMetric());
         Reader testReader = Reader(test, m->getDistMetric());
 
@@ -26,11 +28,9 @@ void UploadCommand::execute()
         m->setTestData(testVector);
         m->setTrainData(trainVector);
     }
-
     else if (!getDIO()->getIOKind().compare(SocketIO::KIND))
     {
         // The input is a string contains the contents of a file
-
         Reader trainReader = Reader(m->getDistMetric());
         Reader testReader = Reader(m->getDistMetric());
 
