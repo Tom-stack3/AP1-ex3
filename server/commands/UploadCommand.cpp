7b583957be3a1745bc23af98168e7cece84a5670
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-18 15:05:27 +0300

    reorganized and added print of "Upload complete."

--- server/commands/UploadCommand.cpp
+++ server/commands/UploadCommand.cpp
@@ -2,45 +2,46 @@
 
 void UploadCommand::execute()
 {
-    getDIO()->write("Please upload your local train CSV file.ֿ");
-
-    std::string train = getDIO()->read();
-    std::cout << train << std::endl;
-
-    getDIO()->write("Please upload your local test CSV file.");
-    std::string test = getDIO()->read();
-    std::cout << "Test:\n" + test << std::endl;
-
     DataManager *m = getDataManager();
+    std::vector<std::shared_ptr<Classified>> trainVector;
+    std::vector<std::shared_ptr<Classified>> testVector;
+
+    getDIO()->write("Please upload your local train CSV file.");
+    std::string train = getDIO()->read();
 
     if (!getDIO()->getIOKind().compare(StandardIO::KIND))
     {
-        // know that the input is a path
+        // The input is a path
         Reader trainReader = Reader(train, m->getDistMetric());
-        Reader testReader = Reader(test, m->getDistMetric());
-
-        std::vector<std::shared_ptr<Classified>> trainVector;
-        std::vector<std::shared_ptr<Classified>> testVector;
-
         trainReader.read(trainVector);
-        testReader.read(testVector);
-
-        m->setTestData(testVector);
-        m->setTrainData(trainVector);
     }
     else if (!getDIO()->getIOKind().compare(SocketIO::KIND))
     {
         // The input is a string contains the contents of a file
         Reader trainReader = Reader(m->getDistMetric());
-        Reader testReader = Reader(m->getDistMetric());
-
-        std::vector<std::shared_ptr<Classified>> trainVector;
-        std::vector<std::shared_ptr<Classified>> testVector;
-
-        trainReader.read(trainVector, train);
-        testReader.read(testVector, test);
-
-        m->setTestData(testVector);
-        m->setTrainData(trainVector);
+        trainReader.read(trainVector);
     }
+
+    getDIO()->write("Upload complete.");
+
+    getDIO()->write("Please upload your local test CSV file.");
+    std::string test = getDIO()->read();
+
+    if (!getDIO()->getIOKind().compare(StandardIO::KIND))
+    {
+        // The input is a path
+        Reader testReader = Reader(test, m->getDistMetric());
+        testReader.read(testVector);
+    }
+    else if (!getDIO()->getIOKind().compare(SocketIO::KIND))
+    {
+        // The input is a string contains the contents of a file
+        Reader testReader = Reader(m->getDistMetric());
+        testReader.read(testVector);
+    }
+
+    getDIO()->write("Upload complete.");
+
+    m->setTestData(testVector);
+    m->setTrainData(trainVector);
 }
