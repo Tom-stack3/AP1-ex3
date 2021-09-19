4e1db5a69182f0580312840fe48c392a7d902a17
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-20 01:25:32 +0300

    trying to add use of timeout in server

--- server/Server.cpp
+++ server/Server.cpp
@@ -1,11 +1,12 @@
 #include "Server.h"
 
-void handleClient(int connectionSocket)
+void handleClient(int connectionSocket, std::vector<int> &sockets)
 {
     Tcp clientSocket;
     Tcp *clientSocket_ptr = &clientSocket;
-
     clientSocket.init(AF_INET, connectionSocket);
+    // Add the socket descriptor to the sockets vector.
+    sockets.push_back(connectionSocket);
 
     Classified::distMetric euc = &EucDistance::getDist;
     DataManager d = DataManager();
@@ -18,6 +19,8 @@ void handleClient(int connectionSocket)
     c.start();
 
     clientSocket.closeSocket();
+    // Remove the socket descriptor from the sockets vector.
+    remove(sockets.begin(), sockets.end(), connectionSocket);
 }
 
 int main()
@@ -27,15 +30,23 @@ int main()
     TcpServer server = TcpServer();
     server.init(AF_INET);
     server.bindSocket("127.0.0.1", port);
+    std::vector<int> clientSockets;
 
     while (true)
     {
         std::cout << "Wating for client..." << std::endl;
         int socketWithClient = server.acceptSocket();
+        // Meaning the timeout was reached, without any clients trying to connect
+        if (socketWithClient == -1)
+        {
+            break;
+        }
+
         std::cout << "Client is connected!" << std::endl;
 
         // We create a new Thread for handling the client.
-        std::thread handlingClient(handleClient, socketWithClient);
+        std::thread handlingClient(handleClient, socketWithClient, clientSockets);
         handlingClient.detach();
     }
+    return 0;
 }
