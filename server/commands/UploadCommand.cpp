fcddcee00592b3a1e5fe17f08e424715d4982f74
Tom-stack3 <tommy.zaft1@gmail.com>
2021-09-22 11:59:43 +0300

    added error sending and processing

--- server/commands/UploadCommand.cpp
+++ server/commands/UploadCommand.cpp
@@ -19,6 +19,14 @@ void UploadCommand::execute()
     {
         // The input is a string contains the contents of a file
         Reader trainReader = Reader(m->getDistMetric());
+        // If an error occured on the client side
+        if (train.compare(TcpServer::CLIENT_ERROR) == 0)
+        {
+            // Exit the communication with the client
+            getDataManager()->setExitStatus(true);
+            getDIO()->write(CLI::EXIT_CODE);
+            return;
+        }
         trainReader.read(trainVector, train);
     }
 
@@ -36,6 +44,14 @@ void UploadCommand::execute()
     {
         // The input is a string contains the contents of a file
         Reader testReader = Reader(m->getDistMetric());
+        // If an error occured on the client side
+        if (test.compare(TcpServer::CLIENT_ERROR) == 0)
+        {
+            // Exit the communication with the client
+            getDataManager()->setExitStatus(true);
+            getDIO()->write(CLI::EXIT_CODE);
+            return;
+        }
         testReader.read(testVector, test);
     }
 
